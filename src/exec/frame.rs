4c0fa30e318bdc4d82af69fff7da8ebc4e4adbfd
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-15 12:16:41 +0100

    Hmm, want the cells in the frame to be references to the actual frame.
    ... I guess we allocate new cells instead of modifying existing ones at the moment?

--- src/exec/frame.rs
+++ src/exec/frame.rs
@@ -3,6 +3,8 @@ use crate::meta::*;
 
 use smallvec::*;
 
+use std::sync::*;
+
 ///
 /// A SAFAS execution frame
 ///
@@ -11,7 +13,7 @@ pub struct Frame {
     previous_frame: Option<Box<Frame>>,
 
     /// Cells allocated to this frame
-    cells: SmallVec<[SafasCell; 8]>,
+    cells: SmallVec<[Arc<SafasCell>; 8]>,
 
     /// The bitcode output of the assembler (bitcode is typically passed between frames, so it's stored by reference)
     bitcode: BitCodeBuffer
