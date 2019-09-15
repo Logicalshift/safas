fc132d2e0641e820d3c90d4bdee74199782eef8c
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-15 14:46:44 +0100

    Add a way to format cells for display

--- src/meta/cell.rs
+++ src/meta/cell.rs
@@ -1,13 +1,16 @@
 use super::number::*;
+use super::atom::*;
 
 use std::sync::*;
+use std::fmt;
+use std::fmt::{Debug, Formatter};
 
 ///
 /// A SAFAS cell represents a single value: for example 'D' or '24'
 /// 
 /// The most complicated of these structures is the list, which just joins two cells
 ///
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub enum SafasCell {
     /// The 'nil' value
     Nil,
@@ -26,4 +29,54 @@ pub enum SafasCell {
 
     /// A list with a CAR and a CDR
     List(Arc<SafasCell>, Arc<SafasCell>)
+}
+
+impl SafasCell {
+    pub fn is_nil(&self) -> bool {
+        match self {
+            SafasCell::Nil  => true,
+            _               => false
+        }
+    }
+
+    ///
+    /// Converts this cell to a string
+    ///
+    pub fn to_string(&self) -> String {
+        use self::SafasCell::*;
+
+        match self {
+            Nil                     => "#nil".to_string(),
+            Atom(atom_id)           => name_for_atom_with_id(*atom_id),
+            Number(number)          => { number.to_string() },
+            String(string_value)    => format!("\"{}\"", string_value),        // TODO: character quoting
+            Char(chr_value)         => format!("'{}'", chr_value),                  // TODO: character quoting,
+            List(first, second)     => {
+                let mut result  = format!("({}", first.to_string());
+                let mut next    = second;
+
+                loop {
+                    match &**next {
+                        Nil                 => { break; },
+                        List(first, second) => {
+                            result.push_str(&format!(" {}", first.to_string()));
+                            next = second;
+                        },
+                        other               => {
+                            result.push_str(&format!(". {}", other.to_string()));
+                            next = second;
+                        }
+                    }
+                }
+
+                result
+            }
+        }
+    }
+}
+
+impl Debug for SafasCell {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(formatter, "{}", self.to_string())
+    }
 }
--- src/meta/number.rs
+++ src/meta/number.rs
@@ -1,3 +1,5 @@
+use radix_fmt::*;
+
 ///
 /// How SAFAS represents a number
 ///
@@ -7,5 +9,32 @@ pub enum SafasNumber {
     Plain(u128),
 
     /// A number that should occupy the specified number of bits
-    BitNumber(u8, u128)
+    BitNumber(u8, u128),
+
+    /// A signed number that should occupy the specified number of bits
+    SignedBitNumber(u8, i128)
+}
+
+impl SafasNumber {
+    ///
+    /// Converts this number to a string representation
+    ///
+    pub fn to_string(&self) -> String {
+        use self::SafasNumber::*;
+
+        match self {
+            Plain(val)              => val.to_string(),
+            BitNumber(bits, num)    => {
+                if *bits < 8 {
+                    format!("{}b{}", radix(*num as u8, 1), bits)
+                } else {
+                    format!("${}u{}", radix(*num, 16), bits)
+                }
+            },
+
+            SignedBitNumber(bits, num)  => {
+                format!("{}i{}", *num, *bits)
+            }
+        }
+    }
 }
