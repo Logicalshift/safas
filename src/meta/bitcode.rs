b14f0ef09d6f50a7b21b3a77de40b0923c794256
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-22 11:57:35 +0100

    Want this to be PartialEq

--- src/meta/bitcode.rs
+++ src/meta/bitcode.rs
@@ -1,7 +1,7 @@
 ///
 /// The output of the assembler is a vector of bitcode, which forms a series of instructions for generating the final result
 /// 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum BitCode {
     /// Write out the specified set of bits and move the file offset forward that much
     Bits(u8, u128),
