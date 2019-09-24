59cef5afa4a57d1ddd04bf4f28ab0e36852f3121
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-24 23:21:50 +0100

    Add a 'syntax compiler' that splits out the 'bind' and the 'compile' steps for a particular statement (we need this for our syntax macros to work properly)

--- src/bind/mod.rs
+++ src/bind/mod.rs
@@ -2,8 +2,10 @@ mod symbol_bindings;
 mod binding_monad;
 mod bind_statement;
 mod bind_error;
+mod syntax_compiler;
 
 pub use self::symbol_bindings::*;
 pub use self::binding_monad::*;
 pub use self::bind_statement::*;
 pub use self::bind_error::*;
+pub use self::syntax_compiler::*;
---
+++ src/bind/syntax_compiler.rs
@@ -0,0 +1,21 @@
+use super::bind_error::*;
+use super::binding_monad::*;
+
+use crate::meta::*;
+use crate::exec::*;
+
+use smallvec::*;
+
+///
+/// A syntax compiler describes the actions needed to compile a piece of syntax into a series of actions
+/// 
+/// There are two components. The binding monad binds all of the cells to their bound values. The action
+/// takes the result of the binding and applies it to generate the actions required to execute the syntax
+///
+pub struct SyntaxCompiler {
+    /// Generates the bound statement for this syntax
+    pub binding_monad: Box<dyn BindingMonad<Binding=Result<CellRef, BindError>>>,
+
+    /// Generates the actions for the bound syntax
+    pub generate_actions: Box<dyn Fn(CellRef) -> Box<dyn BindingMonad<Binding=Result<SmallVec<[Action; 8]>, BindError>>>>
+}
