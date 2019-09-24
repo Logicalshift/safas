f1e5271d08d18ddf93928ceeb9f4f6d283a715c1
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-25 00:23:52 +0100

    Add some cell conversions for turning cells into atom IDs or tuples

--- src/meta/cell_conversion.rs
+++ src/meta/cell_conversion.rs
@@ -2,6 +2,7 @@ use super::cell::*;
 use super::number::*;
 
 use crate::exec::*;
+use crate::bind::*;
 
 use std::result::{Result};
 use std::convert::{TryFrom};
@@ -70,4 +71,58 @@ impl TryFrom<CellRef> for CellValue<i64> {
 impl TryFrom<CellRef> for CellValue<i128> {
     type Error=RuntimeError;
     fn try_from(cell: CellRef) -> Result<Self, RuntimeError> { Ok(CellValue(i128::try_from(SafasNumber::try_from(cell)?)?)) }
+}
+
+///
+/// Represents an atom ID, used for convertsions
+///
+pub struct AtomId(pub u64);
+
+impl TryFrom<CellRef> for AtomId {
+    type Error=RuntimeError;
+
+    fn try_from(cell: CellRef) -> Result<AtomId, RuntimeError> {
+        match &*cell {
+            SafasCell::Atom(val)    => Ok(AtomId(*val)),
+            _                       => Err(RuntimeError::BindingError(BindError::SyntaxExpectingAtom))
+        }
+    }
+}
+
+///
+/// Represents a tuple generated from a list, used for conversions
+///
+pub struct ListTuple<T>(pub T);
+
+impl<A1> TryFrom<CellRef> for ListTuple<(A1, )>
+where   A1:             TryFrom<CellRef>,
+        RuntimeError:   From<A1::Error> {
+    type Error=RuntimeError;
+    fn try_from(cell: CellRef) -> Result<Self, Self::Error> {
+        // Read the list values
+        let (first, cdr) = cell.list_value().ok_or(RuntimeError::BindingError(BindError::SyntaxExpectingList))?;
+
+        if !cdr.is_nil() { return Err(RuntimeError::BindingError(BindError::TooManyArguments)); }
+
+        // Convert them and generate the tuple
+        Ok(ListTuple((A1::try_from(first)?, )))
+    }
+}
+
+impl<A1, A2> TryFrom<CellRef> for ListTuple<(A1, A2)>
+where   A1:         TryFrom<CellRef>,
+        A2:         TryFrom<CellRef>,
+        RuntimeError:   From<A1::Error>,
+        RuntimeError:   From<A2::Error> {
+    type Error=RuntimeError;
+    fn try_from(cell: CellRef) -> Result<Self, Self::Error> {
+        // Read the list values
+        let (first, cdr)    = cell.list_value().ok_or(RuntimeError::BindingError(BindError::SyntaxExpectingList))?;
+        let (second, cdr)   = cdr.list_value().ok_or(RuntimeError::BindingError(BindError::MissingArgument))?;
+
+        if !cdr.is_nil() { return Err(RuntimeError::BindingError(BindError::TooManyArguments)); }
+
+        // Convert them and generate the tuple
+        Ok(ListTuple((A1::try_from(first)?, A2::try_from(second)?)))
+    }
 }
