02170ab7bc2718c9c7a949d218c43f2e18a97c60
Andrew Hunter <andrew@logicalshift.co.uk>
2019-09-24 23:53:20 +0100

    Add a 'read binding arguments' monad

---
+++ src/bind/bind_args_monad.rs
@@ -0,0 +1,52 @@
+use super::bind_error::*;
+use super::binding_monad::*;
+use super::symbol_bindings::*;
+
+use crate::meta::*;
+
+use std::convert::*;
+use std::marker::{PhantomData};
+
+///
+/// Monad that returns the arguments to the syntax being bound
+/// 
+/// Any type that can be converted from a CellRef can be used
+///
+pub struct BindArgsMonad<TArgs> {
+    args: PhantomData<TArgs>
+}
+
+impl<TArgs> BindArgsMonad<TArgs>
+where   TArgs: TryFrom<CellRef>,
+        TArgs: Send+Sync,
+        <TArgs as TryFrom<CellRef>>::Error: Into<BindError> {
+    /// Creates a new BindArgsMonad that will attempt to load the cell into the specified type
+    pub fn new() -> BindArgsMonad<TArgs> {
+        BindArgsMonad {
+            args: PhantomData
+        }
+    }
+}
+
+impl<TArgs> BindingMonad for BindArgsMonad<TArgs> 
+where   TArgs: TryFrom<CellRef>,
+        TArgs: Send+Sync,
+        <TArgs as TryFrom<CellRef>>::Error: Into<BindError> {
+    type Binding = Result<TArgs, BindError>;
+
+    fn resolve(&self, bindings: SymbolBindings) -> (SymbolBindings, Self::Binding) {
+        if let Some(args) = bindings.args.as_ref() {
+            // Try to convert the arguments into the target type
+            let args = args.clone();
+            let args = TArgs::try_from(args);
+
+            match args {
+                Ok(args)    => (bindings, Ok(args)),
+                Err(err)    => (bindings, Err(err.into()))
+            }
+        } else {
+            // No arguments were supplied
+            (bindings, Err(BindError::ArgumentsWereNotSupplied))
+        }
+    }
+}
--- src/bind/mod.rs
+++ src/bind/mod.rs
@@ -1,11 +1,13 @@
 mod symbol_bindings;
 mod binding_monad;
+mod bind_args_monad;
 mod bind_statement;
 mod bind_error;
 mod syntax_compiler;
 
 pub use self::symbol_bindings::*;
 pub use self::binding_monad::*;
+pub use self::bind_args_monad::*;
 pub use self::bind_statement::*;
 pub use self::bind_error::*;
 pub use self::syntax_compiler::*;
