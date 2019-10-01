cfe500fb480528f4e219dfd3f516598877aac88e
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-01 22:45:06 +0100

    `wrap` is itself a monad in this scheme (so '(wrap 1)' is treated as a monad containing '1')

--- src/functions/monad.rs
+++ src/functions/monad.rs
@@ -3,11 +3,18 @@ use crate::meta::*;
 
 ///
 /// `(wrap x)` -> monad wrapping x
+/// 
+/// `wrap` is itself a monad, returning a function that returns a monad that wraps its value (this property makes
+/// its return value a monad to the binder)
 ///
-pub fn wrap_fn() -> impl FrameMonad<Binding=RuntimeResult> {
-    FnMonad::from(|(val, )| {
+pub fn wrap_monad() -> CellRef {
+    // Our monad just returns this wrapping function
+    let wrap_fn = FnMonad::from(|(val, )| {
         let wrap    = WrapFlatMap(val);
         let wrap    = SafasCell::FrameMonad(Box::new(wrap)).into(); 
         SafasCell::Monad(SafasCell::Nil.into(), MonadType::new(wrap)).into()
-    })
+    });
+    let wrap_fn     = SafasCell::FrameMonad(Box::new(wrap_fn)).into();
+
+    SafasCell::Monad(SafasCell::Nil.into(), MonadType::new(wrap_fn)).into()
 }
