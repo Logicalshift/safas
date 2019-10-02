0de8c0004aa46c6136356d98930a8658cb5c2ead
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-02 23:12:09 +0100

    Add some tests for the string values of monads (these will be a bit fragile)

--- src/functions/monad.rs
+++ src/functions/monad.rs
@@ -83,4 +83,52 @@ mod test {
             ).unwrap().0;
         assert!(val.is_monad());
     }
+
+    #[test]
+    fn wrap_monad_value() {
+        let val = eval(
+                "(wrap 1)"
+            ).unwrap().0.to_string();
+        assert!(val == "monad#()#(flat_map: ##wrap(1))".to_string());
+    }
+
+    #[test]
+    fn monad_value_from_list_call_1() {
+        let val = eval(
+                "(list (wrap 1) 2)"
+            ).unwrap().0.to_string();
+        assert!(val == "monad#()#(flat_map: ##wrap((1 2)))".to_string());
+    }
+
+    #[test]
+    fn monad_value_from_list_call_2() {
+        let val = eval(
+                "(list 1 (wrap 2))"
+            ).unwrap().0.to_string();
+        assert!(val == "monad#()#(flat_map: ##wrap((1 2)))".to_string());
+    }
+
+    /*
+    #[test]
+    fn def_monad() {
+        // (def foo (wrap 1)) should produce a value that works like (wrap 1) (ie, which we see as a monad)
+        let val = eval(
+                "(def some_monad (wrap 2))
+                (list 1 some_monad)"
+            ).unwrap().0;
+        assert!(val.is_monad());
+        assert!(val.to_string() == "monad#()#(flat_map: ##wrap((1 2)))".to_string());
+    }
+
+    #[test]
+    fn fun_monad() {
+        // Calling a function that returns a monad should act like a monad in context
+        let val = eval(
+                "(def my_wrap (fun (x) (wrap x)))
+                (list 1 (my_wrap 2))"
+            ).unwrap().0;
+        assert!(val.is_monad());
+        assert!(val.to_string() == "monad#()#(flat_map: ##wrap((1 2)))".to_string());
+    }
+    */
 }
