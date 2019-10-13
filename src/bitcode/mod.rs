7da67ac302f0a24dce7d03fd836884216d66d3bd
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-13 20:44:45 +0100

    Add an empty 'assemble' function to retrieve the bitcode from a bitcode monad

--- src/bitcode/mod.rs
+++ src/bitcode/mod.rs
@@ -2,8 +2,10 @@ mod code;
 mod buffer;
 mod bitcode_monad;
 mod bitcode_functions;
+mod assemble;
 
 pub use self::code::*;
 pub use self::buffer::*;
 pub use self::bitcode_monad::*;
 pub use self::bitcode_functions::*;
+pub use self::assemble::*;
