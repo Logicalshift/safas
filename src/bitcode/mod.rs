c2f00a571df1df2e73d935e385ab7fb718b14362
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-17 23:43:50 +0100

    Rename bitcode_syntax -> label_syntax

--- src/bitcode/mod.rs
+++ src/bitcode/mod.rs
@@ -2,12 +2,12 @@ mod code;
 mod label;
 mod bitcode_monad;
 mod bitcode_functions;
-mod bitcode_syntax;
+mod label_syntax;
 mod assemble;
 
 pub use self::code::*;
 pub use self::label::*;
 pub use self::bitcode_monad::*;
 pub use self::bitcode_functions::*;
-pub use self::bitcode_syntax::*;
+pub use self::label_syntax::*;
 pub use self::assemble::*;
