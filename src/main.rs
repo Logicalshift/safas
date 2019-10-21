b7d730867c5915a48d097bb9a38d2eb37c5bac2f
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-21 22:35:28 +0100

    Add some basic arguments, and make interactive mode an option with '-i'

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,41 @@ mod functions;
 mod interactive;
 
 use self::interactive::*;
+use clap::{App, Arg};
 
 fn main() {
-    run_interactive();
+    // Fetch the parameters
+    let params = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Copyright 2019 Andrew Hunter <andrew@logicalshift.io>")
+        .about("The Self-Aware Functional Assembler")
+        .after_help(concat!("Full source code is available at https://github.com/Logicalshift/safas\n",
+            "\n",
+            "Licensed under the Apache License, Version 2.0 (the \"License\");\n",
+            "you may not use this file except in compliance with the License.\n",
+            "You may obtain a copy of the License at\n",
+            "\n",
+            "http://www.apache.org/licenses/LICENSE-2.0\n\n"))
+        .arg(Arg::with_name("interactive")
+            .short("i")
+            .long("interactive")
+            .help("Launches the interactive interpreter"))
+        .arg(Arg::with_name("INPUT")
+            .help("Sets the input file to read from")
+            .index(1))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .help("Sets the location to write the output to")
+            .value_name("OUTPUT"))
+        .get_matches();
+
+    // Start in interactive mode if the -i parameter is passed in
+    if params.occurrences_of("interactive") > 0 {
+        run_interactive();
+        return;
+    } else {
+        // No valid parameters supplied
+        println!("{}", params.usage());
+    }
 }
