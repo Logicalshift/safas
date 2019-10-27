b311a8cab50ccdc6c051da9aa6bf364c7b3aad0f
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-27 12:32:04 +0000

    Add the start of an import keyword (try to locate the file)

--- src/io/import.rs
+++ src/io/import.rs
@@ -3,7 +3,10 @@ use crate::meta::*;
 use crate::exec::*;
 use crate::parse::*;
 
+use smallvec::*;
 use std::path::{Path, PathBuf, Component};
+use std::convert::{TryFrom};
+use std::sync::*;
 use std::fs;
 
 const DEFAULT_EXTENSION: &str = "sf";
@@ -12,11 +15,20 @@ const DEFAULT_EXTENSION: &str = "sf";
 /// Represents where an imported file can be loaded from
 ///
 pub enum ImportFile {
+    /// Import file was not found
+    NotFound,
+
     /// File to be loaded from a particular path
     FromPath(PathBuf),
 
     // File found in the builtins specified in the bindings
     BuiltIn(String)
+}
+
+impl Default for ImportFile {
+    fn default() -> Self {
+        ImportFile::NotFound
+    }
 }
 
 ///
@@ -42,8 +54,13 @@ fn read_strings(value: Option<(CellRef, u32)>) -> Vec<String> {
 
 ///
 /// Attempts to locate where a particular file to be imported can be loaded from
+/// 
+/// The `filename` is the name provided by the user/program for the file to import. The `bindings` are used to
+/// retrieve the environment for the import: in particular, the `import_path` and `built_ins` atoms should be
+/// defined to be the list of paths to search for imports and the list of built-in definitions respectively.
+/// Search order follows the import paths first then looks at the builtins.
 ///
-pub fn locate_import_file(filename: &str, bindings: &SymbolBindings, allow_relative: bool) -> (Option<ImportFile>) {
+pub fn locate_import_file(filename: &str, bindings: &SymbolBindings, allow_relative: bool) -> ImportFile {
     // The import_path atom can be defined to a list of paths to try to read imported files from
     let import_path = get_id_for_atom_with_name("import_path");
     let built_ins   = get_id_for_atom_with_name("built_ins");
@@ -92,7 +109,11 @@ pub fn locate_import_file(filename: &str, bindings: &SymbolBindings, allow_relat
         found
     };
 
-    file_path.map(|path| ImportFile::FromPath(path))
+    if let Some(file_path) = file_path {
+        ImportFile::FromPath(file_path)
+    } else {
+        ImportFile::NotFound
+    }
 }
 
 ///
@@ -104,7 +125,7 @@ pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow
     let file_path = locate_import_file(filename, &bindings, allow_relative);
 
     // Read the file contents
-    let (file_content, file_path) = if let Some(ImportFile::FromPath(file_path)) = file_path {
+    let (file_content, file_path) = if let ImportFile::FromPath(file_path) = file_path {
         let content = fs::read_to_string(file_path.as_path());
         let content = match content { Ok(content) => content, Err(_err) => { return (RuntimeError::IOError.into(), bindings, frame); } };
 
@@ -120,4 +141,53 @@ pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow
 
     // Evaluate the file
     eval_statements(file_content, NIL.clone(), bindings, frame)
+}
+
+struct LocateImportFile;
+
+impl BindingMonad for LocateImportFile {
+    type Binding = ImportFile;
+
+    fn bind(&self, bindings: SymbolBindings) -> (SymbolBindings, Result<ImportFile, BindError>) {
+        // Fetch the arguments to this expression
+        let args = match bindings.args.as_ref() { Some(args) => args.clone(), None => return (bindings, Err(BindError::MissingArgument)) };
+        let args = ListTuple::<(CellValue<String>, )>::try_from(args);
+        let args = match args { Ok(args) => args, Err(err) => return (bindings, Err(err.into())) };
+
+        let ListTuple((CellValue(filename), )) = args;
+
+        // Locate the file. Implicit relative paths are not allowed when using the (import) syntax
+        let file_path = locate_import_file(&filename, &bindings, false);
+
+        match file_path {
+            // Import file could not be found
+            ImportFile::NotFound => (bindings, Err(BindError::FileNotFound)),
+
+            // Return the file location
+            _ => (bindings, Ok(file_path))
+        }
+    }
+
+    fn pre_bind(&self, bindings: SymbolBindings) -> (SymbolBindings, ImportFile) {
+        // No pre-binding is performed with the import files
+        (bindings, ImportFile::NotFound)
+    }
+}
+
+///
+/// Creates the compiler for the import keyword
+///
+pub fn import_keyword() -> SyntaxCompiler {
+    let bind = LocateImportFile.and_then(|file_path| {
+        wrap_binding(NIL.clone())
+    });
+
+    let compile = |binding| {
+        Ok(CompiledActions::empty())
+    };
+
+    SyntaxCompiler {
+        binding_monad:      Box::new(bind),
+        generate_actions:   Arc::new(compile)
+    }
 }
--- src/syntax/standard_syntax.rs
+++ src/syntax/standard_syntax.rs
@@ -3,6 +3,7 @@ use super::def_syntax::*;
 use super::fun::*;
 use super::quote::*;
 
+use crate::io::*;
 use crate::meta::*;
 use crate::exec::*;
 use crate::bind::*;
@@ -16,6 +17,7 @@ use smallvec::*;
 pub fn standard_syntax() -> impl BindingMonad<Binding=SmallVec<[Action; 8]>> {
     // Define the standard syntax
     let syntax  = wrap_binding(smallvec![]);
+    let syntax  = flat_map_binding_actions(move || define_symbol_value("import",        SafasCell::Syntax(import_keyword(), NIL.clone())), syntax);
     let syntax  = flat_map_binding_actions(move || define_symbol_value("def",           SafasCell::Syntax(def_keyword(), NIL.clone())), syntax);
     let syntax  = flat_map_binding_actions(move || define_symbol_value("def_syntax",    SafasCell::Syntax(def_syntax_keyword(), NIL.clone())), syntax);
     let syntax  = flat_map_binding_actions(move || define_symbol_value("fun",           SafasCell::Syntax(fun_keyword(), NIL.clone())), syntax);
