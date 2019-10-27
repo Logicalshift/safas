951982e88bb76aad0761ace570304f18d062a642
Andrew Hunter <andrew@logicalshift.co.uk>
2019-10-27 11:58:19 +0000

    Extract the 'locate import file' method so we can use it elsewhere

--- src/io/import.rs
+++ src/io/import.rs
@@ -3,10 +3,21 @@ use crate::meta::*;
 use crate::exec::*;
 use crate::parse::*;
 
-use std::path::{Path, Component};
+use std::path::{Path, PathBuf, Component};
 use std::fs;
 
 const DEFAULT_EXTENSION: &str = "sf";
+
+///
+/// Represents where an imported file can be loaded from
+///
+pub enum ImportFile {
+    /// File to be loaded from a particular path
+    FromPath(PathBuf),
+
+    // File found in the builtins specified in the bindings
+    BuiltIn(String)
+}
 
 ///
 /// Reads a value retrieved using look_up from a set of symbol bindings as a series of strings
@@ -42,11 +53,9 @@ fn read_strings(value: Option<(CellRef, u32)>, frame: &Frame) -> Vec<String> {
 }
 
 ///
-/// Imports a file using the specified symbol bindings as the environment (for non-absolute paths, the `import_path` atom can be defined
-/// to a list of places to look. `built_ins` can be used to supply a set of built-in files as strings that are used if the file can't
-/// be found on the import path)
+/// Attempts to locate where a particular file to be imported can be loaded from
 ///
-pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow_current_dir: bool) -> (CellRef, SymbolBindings, Frame) {
+pub fn locate_import_file(filename: &str, bindings: &SymbolBindings, frame: &Frame, allow_relative: bool) -> (Option<ImportFile>) {
     // The import_path atom can be defined to a list of paths to try to read imported files from
     let import_path = get_id_for_atom_with_name("import_path");
     let built_ins   = get_id_for_atom_with_name("built_ins");
@@ -55,15 +64,15 @@ pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow
     let built_ins   = bindings.look_up(built_ins);
 
     // Convert the import path into a list of strings
-    let import_path = read_strings(import_path, &frame);
+    let import_path = read_strings(import_path, frame);
 
     // Try to open the file by searching the input paths
     let file_path = Path::new(filename);
 
-    let file_path = if allow_current_dir && !file_path.is_absolute() && file_path.is_file() {
+    let file_path = if allow_relative && !file_path.is_absolute() && file_path.is_file() {
         // If the current directory is allowed then try the current directory
         Some(file_path.to_path_buf())
-    } else if allow_current_dir && !file_path.is_absolute() && file_path.extension().is_none() && file_path.with_extension(DEFAULT_EXTENSION).is_file() {
+    } else if allow_relative && !file_path.is_absolute() && file_path.extension().is_none() && file_path.with_extension(DEFAULT_EXTENSION).is_file() {
         // We also add the '.sf' extension if it's not already set
         Some(file_path.with_extension(DEFAULT_EXTENSION))
     } else if file_path.is_absolute() || file_path.components().nth(0) == Some(Component::CurDir) || file_path.components().nth(0) == Some(Component::ParentDir) {
@@ -95,8 +104,19 @@ pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow
         found
     };
 
+    file_path.map(|path| ImportFile::FromPath(path))
+}
+
+///
+/// Imports a file using the specified symbol bindings as the environment (for non-absolute paths, the `import_path` atom can be defined
+/// to a list of places to look. `built_ins` can be used to supply a set of built-in files as strings that are used if the file can't
+/// be found on the import path)
+///
+pub fn import_file(filename: &str, bindings: SymbolBindings, frame: Frame, allow_relative: bool) -> (CellRef, SymbolBindings, Frame) {
+    let file_path = locate_import_file(filename, &bindings, &frame, allow_relative);
+
     // Read the file contents
-    let (file_content, file_path) = if let Some(file_path) = file_path {
+    let (file_content, file_path) = if let Some(ImportFile::FromPath(file_path)) = file_path {
         let content = fs::read_to_string(file_path.as_path());
         let content = match content { Ok(content) => content, Err(_err) => { return (RuntimeError::IOError.into(), bindings, frame); } };
 
